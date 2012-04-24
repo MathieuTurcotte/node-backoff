5dece1776e73fc4759201b49435e255f14b56e46
Mathieu Turcotte <turcotte.mat@gmail.com>
2012-04-24 12:33:03 -0400

    Refactoring and increase test coverage.

--- backoff.js
+++ backoff.js
@@ -11,44 +11,59 @@ function ExponentialBackoff(options) {
 
     var options = options || {};
 
-    this.exponentialFactor = options.exponentialFactor || 2;
+    if (options.initialTimeout != undefined && options.initialTimeout < 1) {
+        throw new Error('The initial timeout must be greater than 0.');
+    } else if (options.maxTimeout != undefined && options.maxTimeout < 1) {
+        throw new Error('The maximal timeout must be greater than 0.');
+    }
+
     this.initialTimeout = options.initialTimeout || 100;
     this.maxTimeout = options.maxTimeout || 10000;
 
+    if (this.maxTimeout <= this.initialTimeout) {
+        throw new Error('The maximal timeout must be greater ' +
+                        'than the the initial timeout.');
+    }
+
     this.backoffInProgress = false;
     this.backoffNumber = 0;
+    this.backoffDelay = 0;
     this.timeoutID = -1;
+
+    this.handlers = {
+        backoff: this.onBackoff.bind(this)
+    };
 };
 util.inherits(ExponentialBackoff, events.EventEmitter);
 
-ExponentialBackoff.prototype.calculateBackoffDelay = function() {
-    var multiplicativeFactor = Math.pow(this.exponentialFactor, this.backoffNumber);
-    var delay = Math.min(this.initialTimeout * multiplicativeFactor, this.maxTimeout);
-    return Math.round(delay);
+ExponentialBackoff.prototype.updateBackoffDelay = function() {
+    if (this.backoffDelay < this.maxTimeout) {
+        var multiplicativeFactor = Math.pow(2, this.backoffNumber);
+        var delay = Math.min(this.initialTimeout * multiplicativeFactor, this.maxTimeout);
+        this.backoffDelay = Math.round(delay);
+    }
 };
 
 ExponentialBackoff.prototype.backoff = function() {
     if (this.backoffInProgress) {
-        return false;
-    } else {
-        var delay = this.calculateBackoffDelay();
-        var onBackoff = this.onBackoff.bind(this, delay);
-        this.timeoutID = setTimeout(onBackoff, delay);
-        this.backoffInProgress = true;
-        this.backoffNumber++;
-        return true;
+        throw new Error('Backoff in progress.');
     }
+    this.updateBackoffDelay();
+    this.timeoutID = setTimeout(this.handlers.backoff, this.backoffDelay);
+    this.backoffInProgress = true;
+    this.backoffNumber++;
 };
 
 ExponentialBackoff.prototype.onBackoff = function(delay) {
     this.backoffInProgress = false;
-    this.emit('backoff', this.backoffNumber, delay);
+    this.emit('backoff', this.backoffNumber, this.backoffDelay);
 };
 
 ExponentialBackoff.prototype.reset = function() {
     this.backoffInProgress = false;
     clearTimeout(this.timeoutID);
     this.backoffNumber = 0;
+    this.backoffDelay = 0;
     this.emit('reset');
 };
 
--- tests/backoff.js
+++ tests/backoff.js
@@ -22,7 +22,32 @@ exports["Backoff"] = testCase({
         callback();
     },
 
-    "the 'backoff' event should be emitted on backoff completion": function(test) {
+    "'backoff' event should be emitted on backoff completion": function(test) {
+        var backoff = new Backoff({
+            initialTimeout: 10,
+        });
+        var spy = new sinon.spy();
+        backoff.on('backoff', spy);
+
+        backoff.backoff();
+        this.clock.tick(10);
+
+        test.ok(spy.calledOnce, 'backoff event has not been emitted');
+        test.done();
+    },
+
+    "'reset' event should be emitted on reset": function(test) {
+        var backoff = new Backoff();
+        var reset = sinon.spy();
+        backoff.on('reset', reset);
+
+        backoff.reset();
+
+        test.ok(reset.calledOnce, 'reset event has not been emitted');
+        test.done();
+    },
+
+    "the backoff delay should increase exponentially from initialTimeout to maxTimeout": function(test) {
         var backoff = new Backoff({
             initialTimeout: 10,
             maxTimeout: 1000
@@ -30,32 +55,103 @@ exports["Backoff"] = testCase({
         var spy = new sinon.spy();
         backoff.on('backoff', spy);
 
-        var expectedDelays = [10, 20, 40, 80, 160, 320, 640, 1000];
-        for (var i = 0; i < expectedDelays.length; i++) {
+        var clock = this.clock;
+        var delays = [10, 20, 40, 80, 160, 320, 640, 1000, 1000];
+
+        delays.forEach(function(delay, i) {
             backoff.backoff();
-            this.clock.tick(expectedDelays[i]);
-            test.ok(spy.calledWith(i + 1, expectedDelays[i]));
-            spy.reset();
-        }
+            clock.tick(delay);
+        });
+
+        delays.forEach(function(delay, i) {
+            test.equals(spy.getCall(i).args[0], i + 1);
+            test.equals(spy.getCall(i).args[1], delay);
+        });
 
         test.done();
     },
 
-    "the 'reset' event should be emitted on reset": function(test) {
+    "the initial timeout should be greater than 0": function(test) {
+        test.throws(function() {
+            var backoff = new Backoff({
+                initialTimeout: -1
+            });
+        });
+
+        test.throws(function() {
+            var backoff = new Backoff({
+                initialTimeout: 0
+            });
+        });
+
+        test.doesNotThrow(function() {
+            var backoff = new Backoff({
+                initialTimeout: 1
+            });
+        });
+
+        test.done();
+    },
+
+    "the maximal timeout should be greater than 0": function(test) {
+        test.throws(function() {
+            var backoff = new Backoff({
+                maxTimeout: -1
+            });
+        });
+
+        test.throws(function() {
+            var backoff = new Backoff({
+                maxTimeout: 0
+            });
+        });
+
+        test.done();
+    },
+
+    "the maximal timeout should be greater than the original timeout": function(test) {
+        test.throws(function() {
+            var backoff = new Backoff({
+                initialTimeout: 10,
+                maxTimeout: 10
+            });
+        });
+
+        test.doesNotThrow(function() {
+            var backoff = new Backoff({
+                initialTimeout: 10,
+                maxTimeout: 11
+            });
+        });
+
+        test.done();
+    },
+
+    "call to backoff while a backoff is in progress should throw an error": function(test) {
         var backoff = new Backoff();
-        var reset = sinon.spy();
-        backoff.on('reset', reset);
+        backoff.backoff();
+
+        test.throws(function() {
+            backoff.backoff();
+        });
+
+        test.done();
+    },
+
+    "calling reset when a backoff is in progress should disarm the timeout": function(test) {
+        var backoff = new Backoff({
+            initialTimeout: 10
+        });
+
+        var spy = new sinon.spy();
+        backoff.on('backoff', spy);
+
+        backoff.backoff();
 
         backoff.reset();
+        this.clock.tick(100);   // 'backoff' should not be emitted.
 
-        test.ok(reset.calledOnce);
-        test.done();
-    },
-
-    "call to backoff should be ignored when a backoff is in progress": function(test) {
-        var backoff = new Backoff();
-        test.ok(backoff.backoff() == true);
-        test.ok(backoff.backoff() == false);
+        test.equals(spy.callCount, 0, "backoff timeout did trigger");
         test.done();
     },
 
