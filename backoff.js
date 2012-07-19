bdc63f3be744bd880ca680784231b6e429d9b3fe
Mathieu Turcotte <turcotte.mat@gmail.com>
2012-07-19 18:30:35 -0400

    Mark private members.

--- backoff.js
+++ backoff.js
@@ -21,55 +21,55 @@ function ExponentialBackoff(options) {
         throw new Error('The maximal timeout must be greater than 0.');
     }
 
-    this.initialTimeout = options.initialTimeout || 100;
-    this.maxTimeout = options.maxTimeout || 10000;
+    this.initialTimeout_ = options.initialTimeout || 100;
+    this.maxTimeout_ = options.maxTimeout || 10000;
 
-    if (this.maxTimeout <= this.initialTimeout) {
+    if (this.maxTimeout_ <= this.initialTimeout_) {
         throw new Error('The maximal timeout must be greater ' +
                         'than the the initial timeout.');
     }
 
-    this.backoffInProgress = false;
-    this.backoffNumber = 0;
-    this.backoffDelay = 0;
-    this.timeoutID = -1;
+    this.backoffInProgress_ = false;
+    this.backoffNumber_ = 0;
+    this.backoffDelay_ = 0;
+    this.timeoutID_ = -1;
 
     this.handlers = {
-        backoff: this.onBackoff.bind(this)
+        backoff: this.onBackoff_.bind(this)
     };
 }
 util.inherits(ExponentialBackoff, events.EventEmitter);
 
-ExponentialBackoff.prototype.EXPONENTIAL_FACTOR = 2;
+ExponentialBackoff.prototype.EXPONENT = 2;
 
-ExponentialBackoff.prototype.updateBackoffDelay = function() {
-    if (this.backoffDelay < this.maxTimeout) {
-        var multiplicativeFactor = Math.pow(this.EXPONENTIAL_FACTOR, this.backoffNumber);
-        var delay = Math.min(this.initialTimeout * multiplicativeFactor, this.maxTimeout);
-        this.backoffDelay = Math.round(delay);
+ExponentialBackoff.prototype.updateBackoffDelay_ = function() {
+    if (this.backoffDelay_ < this.maxTimeout_) {
+        var factor = Math.pow(this.EXPONENT, this.backoffNumber_);
+        var delay = Math.min(this.initialTimeout_ * factor, this.maxTimeout_);
+        this.backoffDelay_ = Math.round(delay);
     }
 };
 
 ExponentialBackoff.prototype.backoff = function() {
-    if (this.backoffInProgress) {
+    if (this.backoffInProgress_) {
         throw new Error('Backoff in progress.');
     }
-    this.updateBackoffDelay();
-    this.timeoutID = setTimeout(this.handlers.backoff, this.backoffDelay);
-    this.backoffInProgress = true;
-    this.backoffNumber++;
+    this.updateBackoffDelay_();
+    this.timeoutID_ = setTimeout(this.handlers.backoff, this.backoffDelay_);
+    this.backoffInProgress_ = true;
+    this.backoffNumber_++;
 };
 
-ExponentialBackoff.prototype.onBackoff = function() {
-    this.backoffInProgress = false;
-    this.emit('backoff', this.backoffNumber, this.backoffDelay);
+ExponentialBackoff.prototype.onBackoff_ = function() {
+    this.backoffInProgress_ = false;
+    this.emit('backoff', this.backoffNumber_, this.backoffDelay_);
 };
 
 ExponentialBackoff.prototype.reset = function() {
-    this.backoffInProgress = false;
-    clearTimeout(this.timeoutID);
-    this.backoffNumber = 0;
-    this.backoffDelay = 0;
+    this.backoffInProgress_ = false;
+    clearTimeout(this.timeoutID_);
+    this.backoffNumber_ = 0;
+    this.backoffDelay_ = 0;
     this.emit('reset');
 };
 
