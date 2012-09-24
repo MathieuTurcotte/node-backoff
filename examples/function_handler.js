57d76c642865fcd909d553d36fcf409ec1370532
Mathieu Turcotte <turcotte.mat@gmail.com>
2012-09-23 20:21:23 -0400

    Refactored function handler example.

--- examples/function_handler.js
+++ examples/function_handler.js
@@ -1,27 +1,34 @@
 #!/usr/bin/env node
 
-var assert = require('assert');
+var backoff = require('../index.js'),
+    http = require('http');
 
-var backoff = require('../index.js');
+function get(options, callback) {
+    http.get(options, function(res) {
+        res.setEncoding('utf8');
+        res.data = '';
+        res.on('data', function (chunk) {
+            res.data += chunk;
+        });
+        res.on('end', function() {
+            callback(null, res);
+        });
+        res.on('close', function(err) {
+            callback(err, res);
+        });
+    }).on('error', function(err) {
+        callback(err, null);
+    });
+}
 
-// Contruct a function which will fail on its first two calls.
-var foo = (function() {
-    var args = [[new Error('1'), null],
-                [new Error('2'), null],
-                [null, 'ok']];
+var bget = backoff.wrap(get);
 
-    return function(callback) {
-        callback.apply(null, args.shift());
-    };
-}());
-
-// Wrap the unreliable function in a backoff handler.
-var fooBackoff = backoff.wrap(foo);
-
-// Call the wrapped function which should now succeed.
-fooBackoff(function(err, result, results) {
-    assert.equal(results.length, 3);
-    assert.equal(result, 'ok');
-    assert.ok(!err);
+bget('http://www.iana.org/domains/example/', function(err, res, results) {
+    console.log('Retries: ' + results.length);
+    if (err) {
+        console.log('Error: ' + err.message);
+    } else {
+        console.log('Status: ' + res.statusCode);
+    }
 });
 
